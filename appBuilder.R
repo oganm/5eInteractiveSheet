781b4292151cb7d7001410d58c42e6dba3ea64ce
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-02-17 15:15:25 -0800

    delete svn

--- appBuilder.R
+++ appBuilder.R
@@ -5,15 +5,8 @@ depends = c("googledrive",
             'magrittr',
             'dplyr',
             'diceSyntax',
-            'stringr',
-            'XML',
             'purrr',
             'ogbox',
-            'rmarkdown',
-            'kableExtra',
-            'knitr',
-            'bindrcpp',
-            'animation',
             'glue',
             'shiny',
             'shinythemes',
@@ -28,6 +21,7 @@ unlink('sheet',recursive = TRUE)
 unlink('sheetCI',recursive = TRUE,force = TRUE)
 
 system('svn checkout https://github.com/oganm/import5eChar/trunk/inst/app')
+unlink('app/.svn', recursive = TRUE, force = TRUE)
 file.rename('app','sheet')
 git2r::clone('https://github.com/oganm/import5eChar.git',local_path = 'sheetCI')
 
@@ -50,25 +44,25 @@ compile_iss()
 
 #######################
 
-
-create_app(
-    app_name     = "5eInteractiveSheetCI", 
-    app_repo_url = "https://github.com/oganm/import5eChar",
-    pkgs         = depends,
-    app_dir = 'sheetCI',include_R = TRUE
-)
-
-file.copy('dice_icon.ico','sheetCI/default.ico',overwrite = TRUE)
-file.copy('dice_icon.ico','sheetCI/setup.ico',overwrite = TRUE)
-file.copy('infoafter.txt','sheetCI/infoafter.txt',overwrite = TRUE)
-file.copy('infobefore.txt','sheetCI/infobefore.txt',overwrite = TRUE)
-
-appR = readLines('sheetCI/utils/app.R')
-appR = c('options(ImThePortableClient = TRUE)',appR)
-writeLines(appR,'sheetCI/utils/app.R')
-
-iss = readLines('sheet/5eInteractiveSheet.iss')
-iss[2] %<>% gsub(pattern = '0.0.0',replacement = appVersion,.)
-writeLines(iss,'sheet/5eInteractiveSheet.iss')
-
-compile_iss()
+# 
+# create_app(
+#     app_name     = "5eInteractiveSheetCI", 
+#     app_repo_url = "https://github.com/oganm/import5eChar",
+#     pkgs         = depends,
+#     app_dir = 'sheetCI',include_R = TRUE
+# )
+# 
+# file.copy('dice_icon.ico','sheetCI/default.ico',overwrite = TRUE)
+# file.copy('dice_icon.ico','sheetCI/setup.ico',overwrite = TRUE)
+# file.copy('infoafter.txt','sheetCI/infoafter.txt',overwrite = TRUE)
+# file.copy('infobefore.txt','sheetCI/infobefore.txt',overwrite = TRUE)
+# 
+# appR = readLines('sheetCI/utils/app.R')
+# appR = c('options(ImThePortableClient = TRUE)',appR)
+# writeLines(appR,'sheetCI/utils/app.R')
+# 
+# iss = readLines('sheet/5eInteractiveSheet.iss')
+# iss[2] %<>% gsub(pattern = '0.0.0',replacement = appVersion,.)
+# writeLines(iss,'sheet/5eInteractiveSheet.iss')
+# 
+# compile_iss()
